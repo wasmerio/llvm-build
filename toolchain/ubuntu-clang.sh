28f4e0ca25ef2f3329442671082bec9452d6cf67
Andrew Scheidecker <andrew@scheidecker.net>
2019-09-01 17:10:24 -0400

    Refactored Azure scripts

--- toolchain/ubuntu-clang.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt-get install -y ninja-build clang lld
-export CC=clang
-export CXX=clang++
-export CFLAGS=-fuse-ld=lld
-export CXXFLAGS=-fuse-ld=lld
--- toolchain/windows-vs2019-x64.bat
+++
@@ -1,4 +0,0 @@
-choco install ninja
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
-set CC=cl.exe
-set CXX=cl.exe
